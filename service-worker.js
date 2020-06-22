a2fa7233852e10bed5a377b408aae8005abde1dc
jamespeterjacob <jamespeterjacob@gmail.com>
2020-06-22 16:52:13 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -41,31 +41,41 @@ self.addEventListener('install', function (event) {
     );
 });
 
-self.addEventListener('fetch', event => {
-    const req = event.request;
-    event.respondWith(cacheFirst(req));
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName.startsWith('pages-cache-') && staticCacheName !== cacheName) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
-async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cachedResponse = await cache.match(req);
-    return cachedResponse || fetch(req);
-}
-
-async function networkFirst(req) {
-    const cache = await caches.open(cacheName);
-    try {
-        const fresh = await fetch(req);
-        cache.put(req, fresh.clone());
-        return fresh;
-    } catch (e) {
-        const cachedResponse = await cache.match(req);
-        return cachedResponse;
-    }
-}
-
-async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cachedResponse = await cache.match(req);
-    return cachedResponse || networkFirst(req);
-}
+self.addEventListener('fetch', function (event) {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                console.log('Found ', event.request.url, ' in cache');
+                return response;
+            }
+            console.log('Network request for ', event.request.url);
+            return fetch(event.request).then(function (response) {
+                if (response.status === 404) {
+                    return caches.match('fourohfour.html');
+                }
+                return caches.open(cached_urls).then(function (cache) {
+                    cache.put(event.request.url, response.clone());
+                    return response;
+                });
+            });
+        }).catch(function (error) {
+            console.log('Error, ', error);
+            return caches.match('offline.html');
+        })
+    );
+});
