93d5299b14c71f71adaa862c2a17eb422a7c47d6
jamespeterjacob <jamespeterjacob@gmail.com>
2020-06-22 15:27:46 -0700

    Add files via upload

--- install.js
+++ install.js
@@ -1,30 +1,29 @@
-// CODELAB: Add event listener for beforeinstallprompt event
-window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+<script>
+    let deferredPrompt;
 
-// CODELAB: Add code to save event & show the install button.
-deferredInstallPrompt = evt;
-installButton.removeAttribute('hidden');
-
-// CODELAB: Add code show install prompt & hide the install button.
-deferredInstallPrompt.prompt();
-// Hide the install button, it can't be called twice.
-evt.srcElement.setAttribute('hidden', true);
-
-// CODELAB: Log user response to prompt.
-deferredInstallPrompt.userChoice
-    .then((choice) => {
-      if (choice.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt', choice);
-      } else {
-        console.log('User dismissed the A2HS prompt', choice);
-      }
-      deferredInstallPrompt = null;
+    window.addEventListener('beforeinstallprompt', function(event) {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
     });
 
+    // Installation must be done by a user gesture! Here, the button click
+    btnAdd.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        btnAdd.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+          } else {
+        console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+    });
 
-// CODELAB: Add event listener for appinstalled event
-window.addEventListener('appinstalled', logAppInstalled);
-
-
-// CODELAB: Add code to log the event
-console.log('Ceferin was installed.', evt);
+    </script>
