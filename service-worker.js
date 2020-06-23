134cea117bb4db163bbac94e05a4505a4bdd69ba
jamespeterjacob <jamespeterjacob@gmail.com>
2020-06-23 07:25:38 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,22 @@
-var cached_urls = [
-    'offline.html',
-    'index.html'
+//var cached_urls = [
+//    'offline.html',
+//    'index.html'
     
-];
-var cache_name = 'ceferin_v1';
+//];
+//var cache_name = 'ceferin_v1';
 
 self.addEventListener('install', function (event) {
     event.waitUntil(
-        cache.open(cache_name)
-            .then(function (cache) {
-                return cache.addAll(cached_urls);
-            })
+        caches.open('ceferin_v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/offline.html',
+            ]);
+        })
+            //.then(function (cache) {
+            //    return cache.addAll(cached_urls);
+            //})
     );
 });
 
@@ -28,27 +34,40 @@ self.addEventListener('activate', function (event) {
     );
 });
 
-self.addEventListener('fetch', function (event) {
-    console.log('Fetch event for ', event.request.url);
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            if (response) {
-                console.log('Found ', event.request.url, ' in cache');
-                return response;
-            }
-            console.log('Network request for ', event.request.url);
-            return fetch(event.request).then(function (response) {
-                if (response.status === 404) {
-                    return caches.match('offline.html');
-                }
-                return caches.open(cached_urls).then(function (cache) {
-                    cache.put(event.request.url, response.clone());
+        caches.match(event.request).then((resp) => {
+            return resp || fetch(event.request).then((response) => {
+                return caches.open('ceferin_v1').then((cache) => {
+                    cache.put(event.request, response.clone());
                     return response;
                 });
             });
-        }).catch(function (error) {
-            console.log('Error, ', error);
-            return caches.match('offline.html');
         })
     );
 });
+
+//self.addEventListener('fetch', function (event) {
+//    console.log('Fetch event for ', event.request.url);
+//    event.respondWith(
+//        caches.match(event.request).then(function (response) {
+//            if (response) {
+//                console.log('Found ', event.request.url, ' in cache');
+//                return response;
+//            }
+//            console.log('Network request for ', event.request.url);
+//            return fetch(event.request).then(function (response) {
+//                if (response.status === 404) {
+//                    return caches.match('offline.html');
+//                }
+//                return caches.open(cached_urls).then(function (cache) {
+//                    cache.put(event.request.url, response.clone());
+//                    return response;
+//                });
+//            });
+//        }).catch(function (error) {
+//            console.log('Error, ', error);
+//            return caches.match('offline.html');
+//        })
+//    );
+//});
